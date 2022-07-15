bd6584b4867b51a1dba3700009b3edd2fd316667
chasechri <christensonchase451@gmail.com>
2022-07-15 12:06:21 -0500

    added dependencies and set up server

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+// REDO MODULE 11 WITH CHALLENGE 11 A T THE SAME TIME
 const express = require("express");
-const app = express();
+const fs = require("fs");
+const path = require("path");
+
+const dbNotes = require("./develop/db/db.json");
+
 const PORT = process.env.PORT || 3001;
 
+const app = express();
+
+app.use(express.static("./devlop/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/notes", (req, res) => {});
+
 app.listen(PORT, () => {
-  console.log("app listening on PORT" + PORT);
+  console.log(`Api server now on port ${PORT}!`);
 });
