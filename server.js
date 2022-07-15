e66a01c898c2df4d5d6315fad7f8dbb8137d99f1
chasechri <christensonchase451@gmail.com>
2022-07-15 13:25:58 -0500

    html routes and start to api routes

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-// const util = require("util");
-let uuidv1 = require("uuidv1");
+const util = require("util");
+// let uuidv1 = require("uuidv1");
 
-// const readFileAsync = util.promisify(fs.readFile);
-// const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
-const dbNotesjson = require("./develop/db/db.json");
+// const dbNotesjson = require("./develop/db/db.json");
 
 const PORT = process.env.PORT || 3001;
 
@@ -18,49 +18,76 @@ app.use(express.static("./devlop/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.get('/api/notes', (req, res) {
-//   readFileAsync("./develop/db/db.json", "UTF-8").then((data), => {
-//     notes
-//   })
-// })
+// api routes
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+app.get("/api/notes", function (req, res) {
+  readFileAsync("./develop/db/db.json", "utf-8").then(function (data) {
+    notes = [].concat(JSON.parse(data));
+    res.json(notes);
+  });
 });
 
-app.get("/api/notes", (req, res) => {
-  const noteData = fs.readFileSync(
-    path.join(__dirname, "./develop/db/db.json"),
-    "utf-8"
-  );
-  const parNote = JSON.parse(noteData);
-  res.json(parNote);
-});
-
-app.post("/api/notes", (req, res) => {
-  const noteData = fs.readFileSync(
-    (path.join(__dirname, "./develop/db/db.json"), "UTF-8")
-  );
-  const parNote = JSON.parse(noteData);
-  req.body.id = uuidv1();
-  parNote.push(req.body);
-
-  fs.writeFileSync(
-    path.join(__dirname, "./develop/db/db.json"),
-    JSON.stringify(parNote),
-    "UTF-8"
-  );
-  res.json("You have successfully added a note!");
-});
-
-// app.post("/api/notes");
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
-});
+app.post();
 
 app.delete();
 
-app.listen(PORT, () => {
-  console.log(`Api server now on port ${PORT}!`);
+// html routes
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
 });
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log("Api server now on port " + PORT);
+});
+
+// not working code
+
+// app.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+// });
+
+// app.get("/api/notes", (req, res) => {
+//   const noteData = fs.readFileSync(
+//     path.join(__dirname, "./develop/db/db.json"),
+//     "utf-8"
+//   );
+//   const parNote = JSON.parse(noteData);
+//   res.json(parNote);
+// });
+
+// app.post("/api/notes", (req, res) => {
+//   const noteData = fs.readFileSync(
+//     (path.join(__dirname, "./develop/db/db.json"), "UTF-8")
+//   );
+//   const parNote = JSON.parse(noteData);
+//   req.body.id = uuidv1();
+//   parNote.push(req.body);
+
+//   fs.writeFileSync(
+//     path.join(__dirname, "./develop/db/db.json"),
+//     JSON.stringify(parNote),
+//     "UTF-8"
+//   );
+//   res.json("You have successfully added a note!");
+// });
+
+// // app.post("/api/notes");
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+// });
+
+// app.delete();
+
+// app.listen(PORT, () => {
+//   console.log(`Api server now on port ${PORT}!`);
+// });
