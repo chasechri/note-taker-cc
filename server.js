9e5a92a4326f47a2b811ae0050a901fe111776e9
chasechri <christensonchase451@gmail.com>
2022-09-09 10:44:42 -0500

    gets

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
+const PORT = process.env.PORT || 3001;
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+const app = express();
+
+const notes = require("./db/db.json");
+
+app.use(express.urlencoded({ extnended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes.slice(1));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
